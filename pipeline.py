0fec9cd49db3f18aec2f5df09b7f44b25e6c1ab2
Karun Arora <karunarora007@gmail.com>
2023-01-28 00:58:48 +0530

    pipeline script

---
+++ pipeline.py
@@ -0,0 +1,12 @@
+import sys
+
+# import pandas as pd
+
+print(sys.argv)
+
+day = sys.argv[1]
+#day = "2023-01-23"
+
+# some fancy stuff with pandas
+
+print(f'job finished successfully for the day = {day}')
